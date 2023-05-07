da8a06560faefe2e23c0f53d1080042bb14945af
EbonyDunlop <ebonydunlop02@hotmail.com>
2023-05-07 14:48:32 +0100

    Add logo and nav bar colour changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Pages from "./pages/Pages";
 import "./App.css";
+import Logo from "./assets/images/Logo.png"
 
 function App() {
   return (
     <div className="App">
-      <h1 id="site-name">The Recipe Router</h1>
+      <div className="logo-container">
+        <img src={Logo} alt="The Recipe Router Logo" id="logo" />
+        <h1 id="site-name">The Recipe Router</h1>
+        <img src={Logo} alt="The Recipe Router Logo" id="logo" />
+      </div>
       <Pages />
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,23 @@
 function NavBar() {
+    const handleMouseOver = (event) => {
+      event.target.style.color = "#8BC34A";
+    };
+  
+    const handleMouseOut = (event) => {
+      event.target.style.color = "black";
+    };
+  
     return (
-        <nav className="navigation-bar">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Recipe Index</li>
-                <li>Contact Us</li>
-            </ul>
-        </nav>
-    )
-}
-
-export default NavBar
+      <nav className="navigation-bar">
+        <ul>
+          <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Home</li>
+          <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>About Us</li>
+          <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Recipe Index</li>
+          <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Contact Us</li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
+  
